2371188d0b92492bd54c5407f740cde9e097f632
Tyler Paneitz <Tyler.Paneitz@vu.com>
2023-02-13 15:03:02 -0600

    Submitted assignment

--- compoundInterest.py
+++ compoundInterest.py
@@ -1,11 +1,22 @@
-def calculateCompoundInterest():
-    
-# This first 3 lines are provided for yougetACompoundInterest()
-# This first 3 lines are provided for you
- client_one_principal = float(input("Principle (amount): "))
- client_one_time =      float(input("Time:               "))
- client_one_rate =      float(input("Rate:               "))
- #print("Compound Interest: "+str(intrest))
+def calculateCompoundInterest():    
+    # This first 3 lines are provided for yougetACompoundInterest()
+    # This first 3 lines are provided for you    
+    print("Compound Interest: "+ compoundInterestCaculator())
+    print('---')
+    print("Compound Interest: "+ compoundInterestCaculator())
+    print('---')
+    print("Compound Interest: " + compoundInterestCaculator())
+    return
+
+def compoundInterestCaculator():
+    client_one_principal = float(input("Principle (amount): "))
+    client_one_time =      float(input("Time:               "))
+    client_one_rate =      float(input("Rate:               "))
+    amount = client_one_principal * ( 1 + client_one_rate/100)**client_one_time
+
+    interest = amount - client_one_principal
+    interest = round(interest,2)
+    return (str(interest))
 
     # end assignment
 
